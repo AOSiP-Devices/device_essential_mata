696aede0bb9d16689e80d3d852c066225afbd8d2
Ethan Chen <intervigil@gmail.com>
2017-12-01 22:51:10 -0800

    mata: Remove QC effects
    * Modules are present in the vendor image, don't duplicate it.
    
    Change-Id: I4c09dae72cb252c01008c9f6625e24b2e8d855c0

--- device.mk
+++ device.mk
@@ -14,10 +14,6 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
-    libqcompostprocbundle \
-    libqcomvisualizer \
-    libqcomvoiceprocessing \
-    libvolumelistener \
     tinymix
 
 # Boot animation
