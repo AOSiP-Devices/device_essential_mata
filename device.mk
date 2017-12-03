05c376089973befa5b8266b4d735ac13a0198e07
Ethan Chen <intervigil@gmail.com>
2017-12-02 16:34:14 -0800

    mata: Remove audio HAL
    * This is part of the vendor image.
    
    Change-Id: I61128c9c2b4afb7903c4e5b215c8c1174df6bf60

--- device.mk
+++ device.mk
@@ -9,7 +9,6 @@ PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
-    audio.primary.msm8998 \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
