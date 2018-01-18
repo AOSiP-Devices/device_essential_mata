81858d3b0ea1eb472834712df35c0715759b03ab
xlxfoxxlx <joshfox87@gmail.com>
2018-01-18 16:46:03 -0500

    Use PixelGapps
    Change-Id: I27357a16b1ecc2cdf0842e3644685ebc10a59f5b

--- device.mk
+++ device.mk
@@ -166,8 +166,7 @@ PRODUCT_COPY_FILES += \
     device/essential/mata/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl
 
 # GApps
-GAPPS_VARIANT := nano
-$(call inherit-product-if-exists, vendor/opengapps/build/opengapps-packages.mk)
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
