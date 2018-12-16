ProtonAOSP/android_device_google_wahoo

Remove system and vendor image dependency
libnfc-nci.conf shouldn't be a part of vendor image, since it is
accessed by libnfc-nci and Nfc application, both are part of system
image.

Test: Nfc enable/disable
Bug: 72080121
Change-Id: I686ba234c7d2aa923070c3fa980c56b73b18e574
(cherry picked from commit 3197b56882ed9997043d87e6582f6ebaa0fdbace)

device.mk
@@ -352,7 +352,7 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-service
 
 PRODUCT_COPY_FILES += \
-    device/google/wahoo/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    device/google/wahoo/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
 
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.1-service.wahoo

AOSiP-Devices/device_essential_mata

Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

device.mk
@@ -256,7 +256,7 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
-    device/essential/mata/configs/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    device/essential/mata/configs/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
     device/essential/mata/configs/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # OMX