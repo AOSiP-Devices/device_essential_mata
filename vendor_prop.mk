437c1a3d26450b3c6581faaf98f1f257263a9821
aagu <aaguisme@gmail.com>
2019-01-26 03:09:08 +0800

    fix merge error

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -136,10 +136,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.default_network=10 \
     telephony.lteOnCdmaDevice=1
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 43eb3bc... mata: update mixer paths
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qti.sensors.dev_ori=false \
